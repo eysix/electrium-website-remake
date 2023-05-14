0a31bbdcfc20c5c023d2dfd62188688c5039d5f9
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2023-05-14 17:13:35 -0400

    set up

---
+++ data/faq.ts
@@ -0,0 +1,15 @@
+export const faq = [
+    {
+        question: "What is Electrium Mobility?",
+        answer: "Electrium Mobility is a design team that teaches students how to build electric vehicles."
+    },
+    {
+        question: "I'm a complete beginner. Can I still join Electrium?",
+        answer: "Yes! People of all skill levels are welcome to join!"
+    },
+]
+
+export interface QA {
+    question: string,
+    answer: string
+}
